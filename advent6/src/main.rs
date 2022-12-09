7012ae2b7525ec35f9bfcf31a44d3eb39de78e0b
Chad Gauthier <chad.gauthier32@gmail.com>
2022-12-09 00:31:44 -0500

    added day 8

--- advent6/src/main.rs
+++ advent6/src/main.rs
@@ -26,8 +26,6 @@ fn find_unique(text: String, length: u32) -> u32 {
 }
 
 fn main() {
-    let start: DateTime<Local> = Local::now();
-
     let mut length: u32 = 14;
     let args: Vec<String> = env::args().collect();
     if args.len() >= 2 {
@@ -37,7 +35,4 @@ fn main() {
     let file: String = read_file("input.txt").unwrap();
     let pos: u32 = find_unique(file, length);
     println!("{}", pos);
-    let end: DateTime<Local> = Local::now();
-    let time: Duration = end - start;
-    println!("Time elapsed: {}ms", time.num_milliseconds());
 }
